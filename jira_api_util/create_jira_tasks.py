636c37eee34d3ca84ecb23c6942e25406f434610
Artem <agp22888@gmail.com>
2025-02-03 20:07:21 +0300

    finished jira population method

--- jira_api_util/create_jira_tasks.py
+++ jira_api_util/create_jira_tasks.py
@@ -1,3 +1,5 @@
+from lorem_text import lorem
+
 from jira_api_util.jira_utils import JiraAPIHelper
 from jira_api_util.utils import LoremFlickrManager, ImageManager
 
@@ -5,20 +7,29 @@ from jira_api_util.utils import LoremFlickrManager, ImageManager
 def main(helper: JiraAPIHelper, image_manager: ImageManager):
     project_num = 3
     issues_per_project = 5
-    comments_per_project = 5
-    project_dicts = []
+    attachments_per_issue = 5
+    projects = []
     for i in range(project_num):
-        project_dicts.append(helper.create_project(f'TEST-PROJECT{i}',
-                                                   'software',
-                                                   'admin'))
-    issue_dicts = []
-    for pd in project_dicts:
+        projects.append(helper.create_project(f'TESTPR{i}',
+                                              'software',
+                                              'admin'))
+    issues = []
+    for project in projects:
         for i in range(issues_per_project):
-            pass  # add issue to project
-
-    for issue in issue_dicts:
-        for i in range(comments_per_project):
-            pass  # add attachment and comment with link to attachment (optional)
+            issues.append(helper.create_issue(project.key,
+                                              lorem.words(2),
+                                              lorem.words(5),
+                                              'Task'))
+    for issue in issues:
+        attachments = []
+        for i in range(attachments_per_issue):
+            img_bytes = image_manager.get_random_image(300, 300)
+            attachments.append(helper.add_attachment(issue.id,
+                                                     img_bytes,
+                                                     f'{lorem.words(1)}.png'))
+        for attachment in attachments:
+            helper.add_comment_to_issue(issue.id,
+                                        f'{lorem.sentence()} !{attachment.filename}!')
 
 
 if __name__ == '__main__':
