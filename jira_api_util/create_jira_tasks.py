1a41eaea41b894c6ad42ebd851ae84da2674df9c
Artem <agp22888@gmail.com>
2025-02-07 21:43:16 +0300

    refactoring

--- jira_api_util/create_jira_tasks.py
+++ jira_api_util/create_jira_tasks.py
@@ -4,13 +4,14 @@ from dotenv import load_dotenv
 from lorem_text import lorem
 
 from jira_api_util.jira_utils import JiraAPIHelper
-from jira_api_util.utils import LoremFlickrManager, ImageManager
+from jira_api_util.utils import LoremFlickrManager, AbstractImageManager
 
 
-def main(helper: JiraAPIHelper, image_manager: ImageManager):
-    project_num = 3
-    issues_per_project = 5
-    attachments_per_issue = 5
+def main(helper: JiraAPIHelper,
+         image_manager: AbstractImageManager,
+         project_num: int,
+         issues_per_project: int,
+         attachments_per_issue: int):
     projects = []
     for i in range(project_num):
         projects.append(helper.create_project(f'TESTPR{i}',
@@ -37,8 +38,34 @@ def main(helper: JiraAPIHelper, image_manager: ImageManager):
 
 if __name__ == '__main__':
     load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
-    h = JiraAPIHelper(os.environ.get('BASE_URL', ''),
-                      os.environ.get('JIRA_ADMIN', ''),
-                      os.environ.get('JIRA_PASSWORD', ''))
-    im = LoremFlickrManager()
-    main(h, im)
+    base_url = os.environ.get('BASE_URL')
+    jira_admin = os.environ.get('JIRA_ADMIN')
+    jira_password = os.environ.get('JIRA_PASSWORD')
+    try:
+        image_download_timeout = int(os.environ.get('IMAGE_DOWNLOAD_TIMEOUT'))
+        project_num = int(os.environ.get('PROJECTS_NUM'))
+        issues_per_project = int(os.environ.get('ISSUES_PER_PROJECT'))
+        attachments_per_project = int(os.environ.get('COMMENTS_WITH_ATTACHMENTS_PER_ISSUE'))
+    except (ValueError, TypeError):
+        print('Значение таймаута, номера проектов, задач и вложений должно быть числовым!')
+        exit(1)
+    if not all((base_url,
+                jira_admin,
+                jira_password,
+                image_download_timeout,
+                project_num,
+                issues_per_project,
+                attachments_per_project)):
+        print("Проверьте значения параметров!")
+        exit(1)
+
+    h = JiraAPIHelper(base_url,
+                      jira_admin,
+                      jira_password)
+    im = LoremFlickrManager(image_download_timeout)
+
+    # main(h,
+    #      im,
+    #      project_num,
+    #      issues_per_project,
+    #      attachments_per_project)
